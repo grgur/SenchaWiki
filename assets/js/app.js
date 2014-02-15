ab9cfb165a6fc3eefc0055570285585e93925765
Grgur Grisogono <grguru@gmail.com>
2014-02-15 14:21:48 +0100

    Caching disqus and addthis

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,11 +66,6 @@ var disqus_shortname = 'grgurwiki'; // required: replace example with your forum
 
 
 //addthis
-(function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52ff6814440a1f04';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
 addthis.layers({
   'theme' : 'transparent',
   'share' : {
